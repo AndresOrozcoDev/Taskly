c4a0d9b3cea38e0dd9d9c35c2cb8b8b80453eb7d
Andres Orozco Dev <andres.orozco.dev@gmail.com>
2025-02-27 12:53:36 -0500

    create forms auth

---
+++ taskly-frontend/src/app/features/auth/components/forget-form/forget-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forget-form',
+  imports: [],
+  templateUrl: './forget-form.component.html',
+  styleUrl: './forget-form.component.scss'
+})
+export class ForgetFormComponent {
+
+}
